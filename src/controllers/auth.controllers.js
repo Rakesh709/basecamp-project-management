8eaff86c3e4290939454563973d91e3eba7bcce7
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-08-09 21:59:07 +0530

    validator , middleware

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -51,7 +51,7 @@ const registerUser = asyncHandler(async (req,res) => {
     subject:"Please verify your email",
     mailgenContent:emailVeficationMailgenContent(
         user.username,
-        `{req.protocol}://${req.get("host")}/api/v1/users/verify-email/${unHashedToken}`
+        `${req.protocol}://${req.get("host")}/api/v1/auth/verify-email/${unHashedToken}`
     )
    });
 
